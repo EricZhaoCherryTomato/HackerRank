b2af4694eed1748a573c01005db5e99f7f99ac75
Eric.Zhao <Eric.Zhao@dominos.com.au>
2016-12-14 11:41:44 +1000

    Day 8: Dictionaries and Maps

--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -10,6 +10,27 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var phoneBook = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                var inputs = Console.ReadLine()?.Split(' ');
+                phoneBook.Add(inputs[0], inputs[1]);
+            }
+            while (n != 0)
+            {
+                var inputs = Console.ReadLine();
+                string value;
+                if (phoneBook.TryGetValue(inputs, out value))
+                {
+                    Console.WriteLine(inputs + "=" + value);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+                n--;
+            }
         }
     }
 }
