18ef80ac04e861da858a0a9f079f8f53cef2a4b9
Eric.Zhao <Eric.Zhao@dominos.com.au>
2016-12-14 15:34:11 +1000

    Day 9: Recursion

--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -1,36 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HackerRank
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var n = Convert.ToInt32(Console.ReadLine());
-            var phoneBook = new Dictionary<string, string>();
-            for (int i = 0; i < n; i++)
+            var r = Factorial(n);
+            Console.WriteLine(r);
+        }
+
+        public static long Factorial(int n)
+        {
+            if (n != 1)
             {
-                var inputs = Console.ReadLine()?.Split(' ');
-                phoneBook.Add(inputs[0], inputs[1]);
+                return n*Factorial(n - 1);
             }
-            while (n != 0)
-            {
-                var inputs = Console.ReadLine();
-                string value;
-                if (phoneBook.TryGetValue(inputs, out value))
-                {
-                    Console.WriteLine(inputs + "=" + value);
-                }
-                else
-                {
-                    Console.WriteLine("Not found");
-                }
-                n--;
-            }
+            return n;
         }
     }
 }
