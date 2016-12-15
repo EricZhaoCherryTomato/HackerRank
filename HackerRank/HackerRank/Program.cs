1644510cfa829795d4b58f11ab61ecb7f4e6eb80
Eric.Zhao <Eric.Zhao@dominos.com.au>
2016-12-15 10:05:30 +1000

    Day 10: Binary Numbers

--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -1,23 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HackerRank
 {
     internal class Program
     {
+        public static List<int> base2 = new List<int>();
+
         private static void Main(string[] args)
         {
             var n = Convert.ToInt32(Console.ReadLine());
-            var r = Factorial(n);
-            Console.WriteLine(r);
+
+            var r = DecimalToBase2(n);
+
+            var index = 1;
+            var counter = new List<int>();
+            for (var i = 1; i < r.Count; i++)
+            {
+                if (r[i] - r[i - 1] == 0)
+                {
+                    index++;
+                    counter.Add(index);
+                }
+                else
+                {
+                    counter.Add(index);
+                    index = 1;
+                }
+            }
+            Console.WriteLine(counter.Max());
         }
 
-        public static long Factorial(int n)
+        public static List<int> DecimalToBase2(int n)
         {
-            if (n != 1)
+            if (n != 0)
             {
-                return n*Factorial(n - 1);
+                base2.Add(n%2 != 0 ? 1 : 0);
+                return DecimalToBase2(n/2);
             }
-            return n;
+            return base2;
         }
     }
 }
