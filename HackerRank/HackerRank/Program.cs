d5375b475c8c568fa074879621cc65b6a7a266fe
Eric.Zhao <Eric.Zhao@dominos.com.au>
2016-12-16 10:51:08 +1000

    Day 16: Exceptions - String to Integer

--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace HackerRank
 {
@@ -10,53 +9,18 @@ namespace HackerRank
 
         private static void Main()
         {
-            Node head = null;
-            int T = Int32.Parse(Console.ReadLine());
-            while (T-- > 0)
+            string S = Console.ReadLine();
+            try
             {
-                int data = Int32.Parse(Console.ReadLine());
-                head = insert(head, data);
+                var r = Convert.ToInt32(S);
+                Console.WriteLine(r);
             }
-            display(head);
-        }
-
-        public static Node insert(Node head, int data)
-        {
-            if (head == null)
+            catch (Exception)
             {
-                return new Node(data);
-            }
-            if (head.next == null)
-            {
-                head.next = new Node(data);
-            }
-            else
-            {
-                insert(head.next, data);
-            }
-            return head;
-
-        }
-
-        internal class Node
-        {
-            public int data;
-            public Node next;
-            public Node(int d)
-            {
-                data = d;
-                next = null;
-            }
-
-        }
-        public static void display(Node head)
-        {
-            Node start = head;
-            while (start != null)
-            {
-                Console.Write(start.data + " ");
-                start = start.next;
+                Console.WriteLine("Bad String");
+               
             }
         }
+       
     }
 }
