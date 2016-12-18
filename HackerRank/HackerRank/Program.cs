635bbeec2c770849c8d21549a576d17174d26cfb
Eric.Zhao <Eric.Zhao@dominos.com.au>
2016-12-18 17:42:29 +1000

    Day 22: Binary Search Trees

--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace HackerRank
 {
@@ -6,19 +7,63 @@ namespace HackerRank
     {
         private static void Main()
         {
-            int[] vInt = {1, 2, 3};
-            string[] vString = {"Hello", "World"};
-
-            printArray<int>(vInt);
-            printArray<string>(vString);
+            Node root = null;
+            var T = int.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                var data = int.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            var height = getHeight(root);
+            Console.WriteLine(height);
         }
 
-        private static void printArray<T>(T[] vArray)
+        private static Node insert(Node root, int data)
         {
-            foreach (var t in vArray)
+            if (root == null)
             {
-                Console.WriteLine(t);
+                return new Node(data);
             }
+            Node cur;
+            if (data <= root.data)
+            {
+                cur = insert(root.left, data);
+                root.left = cur;
+            }
+            else
+            {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+
+        private static int getHeight(Node root)
+        {
+            int leftHeight = 0;
+            int rightHeight = 0;
+
+            if (root.left != null)
+            {
+                leftHeight = getHeight(root.left) + 1;
+            }
+            if (root.right != null)
+            {
+                rightHeight = getHeight(root.right) + 1;
+            }
+            return leftHeight > rightHeight ? leftHeight : rightHeight;
+        }
+    }
+
+    internal class Node
+    {
+        public Node left, right;
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
         }
     }
 }
