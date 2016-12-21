483b696308c67dee2b8fab89bc5c3e9587bbf321
Eric.Zhao <eric.ye.zhao@gmail.com>
2016-12-21 13:47:36 +1000

    Arrays: Left Rotation

--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -6,16 +6,29 @@ namespace HackerRank
 {
     internal class Program
     {
+        //Input Format
+        //The first line contains two space-separated integers denoting the respective values of(the number of integers) and(the number of left rotations you must perform). 
+        //The second line contains  space-separated integers describing the respective elements of the array's initial state.
+
+        //Output Format
+        //Print a single line of space-separated integers denoting the final state of the array after performing left rotations.
         private static void Main()
         {
-            //print all the odd number using bitwise operation
-            for (int i = 0; i < 100; ++i)
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            for (int i = 0; i < n; i++)
             {
-                if ((i & 1) == 1)
-                {
-                     Console.WriteLine(i);
-                }
+                int newLocation = (i + (n - k)) % n;
+                a[newLocation] = a[i];
             }
+            foreach (var i in a)
+            {
+                Console.Write(i + " ");
+            }
+
         }
     }
 }
